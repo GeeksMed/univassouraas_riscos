f64d108396e51f6729c36463c5b6eafcb570f65d
GeeksMed <silva.germain@gmail.com>
2023-05-13 16:07:20 -0300

    Adicionando Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
+import logouni from './img/logo-no-background.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Navbar, Nav} from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" className="align-items-center justify-content-end">
+        <Container>
+          <Navbar.Brand href="#home" sticky="top" className="align-items-center justify-content-end">
+            <img
+              alt=""
+              src={logouni}
+              width="90"
+              className="d-inline-block align-top"
+            />{' '}
+            <Navbar.Text className="align-items-center justify-content-end">React Bootstrap</Navbar.Text>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
     </div>
   );
 }
