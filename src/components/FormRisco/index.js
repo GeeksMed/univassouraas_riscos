42d7845448f4bf8786e37d03032e7ecf6b9a343f
yangianini <yangianini@gmail.com>
2023-06-15 23:22:55 -0300

    form com postzin

--- src/components/FormRisco/index.js
+++ src/components/FormRisco/index.js
@@ -4,7 +4,11 @@ import Form from 'react-bootstrap/Form';
 import {Button} from "react-bootstrap";
 
 export default function FormRisco() {
+    const baseURL = "http://localhost:3000/chamados";
     const [grupoRisco, setGrupoRisco] = useState([]);
+    const [ambiente, setAmbiente] = useState('');
+    const [tipoRisco, setTipoRisco] = useState('');
+    const [observacao, setObservacao] = useState('');
 
     useEffect(() => {
         fetchGrupoRisco();
@@ -20,26 +24,48 @@ export default function FormRisco() {
         }
     };
 
+    async function postData() {
+        const postData = {
+            ambiente: ambiente,
+            riscoId: parseFloat(tipoRisco),
+            observacao: observacao,
+        };
+
+        console.log(postData);
+
+        try {
+            await fetch(`${baseURL}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(postData),
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <Form>
             <Form.Group>
                 <Form.Label>Ambiente: </Form.Label>
-                <Form.Control type='text' placeholder='....' />
+                <Form.Control type='text' placeholder='....' value={ambiente} onChange={(e) => setAmbiente(e.target.value)}/>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Tipo de Risco: </Form.Label>
-                <Form.Select>
+                <Form.Select onChange={(e) => setTipoRisco(e.target.value)}>
                 {grupoRisco?.map((risco, index) => (
                     <option value={risco.id}>{risco.nome}</option>
                 ))}
                 </Form.Select>
             </Form.Group>
             <Form.Group>
-                <Form.Label>Observação: </Form.Label>
-                <Form.Control type='text' placeholder='....' />
+                <Form.Label >Observação: </Form.Label>
+                <Form.Control type='text' placeholder='....' value={observacao} onChange={(e) => setObservacao(e.target.value)} />
             </Form.Group>
             <div className="d-flex my-3 justify-content-center">
-                <Button className="reportar" type={'subimit'}>Reportar Risco</Button>
+                <Button className="reportar" onClick={postData}>Reportar Risco</Button>
             </div>
         </Form>
     )
